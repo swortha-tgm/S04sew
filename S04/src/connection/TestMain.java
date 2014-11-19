cc9a8bbe8e8e927983190d282bac0e399466194a
swortha-tgm <swortha@tgm.ac.at>
2014-11-19 09:27:39 +0100

    add TestMain

---
+++ S04/src/connection/TestMain.java
@@ -0,0 +1,10 @@
+package connection;
+
+public class TestMain {
+	public static void main(String[] args) {
+		Thread server = new Thread(new ChatServer(6666));
+		server.start();
+		Thread client = new Thread(new ChatClient("", 6666));
+		client.start();
+	}
+}
