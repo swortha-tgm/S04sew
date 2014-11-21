3de5b885597a610669ab6be63bfa0957073fb079
swortha-tgm <swortha@tgm.ac.at>
2014-11-21 11:20:00 +0100

    small change in View

--- S04/src/gui/View.java
+++ S04/src/gui/View.java
@@ -7,7 +7,6 @@ import java.awt.event.ActionEvent;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
