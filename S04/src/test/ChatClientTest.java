a8bc7999f852dc2a48cbefd71ecc21e696136f77
sschober-tgm <sschober@student.tgm.ac.at>
2014-11-22 22:14:41 +0100

    Added more test cases

--- S04/src/test/ChatClientTest.java
+++ S04/src/test/ChatClientTest.java
@@ -7,6 +7,7 @@ import gui.View;
 import java.io.IOException;
 import java.net.UnknownHostException;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
@@ -22,12 +23,12 @@ public class ChatClientTest {
 
 	@Test
 	public void testHostname() throws UnknownHostException, IOException{
-		Controller c = new Controller("127.0.0.1", 1234);
+		Controller c = new Controller("127.0.0.1", 123);
 		View v = new View(c);
 		
-		ChatServer server = new ChatServer(1234, v);
+		ChatServer server = new ChatServer(123, v);
 		
-		ChatClient chat = new ChatClient("127.0.0.1", 1234);
+		ChatClient chat = new ChatClient("127.0.0.1", 123);
 		
 		assertEquals("127.0.0.1", chat.getHostName());
 	}
@@ -35,15 +36,30 @@ public class ChatClientTest {
 
 	@Test
 	public void testPort() throws UnknownHostException, IOException{
-		Controller c = new Controller("127.0.0.1", 1234);
+		Controller c = new Controller("127.0.0.1", 12);
 		View v = new View(c);
 		
-		ChatServer server = new ChatServer(1234, v);
+		ChatServer server = new ChatServer(12, v);
 		
-		ChatClient chat = new ChatClient("127.0.0.1", 1234);
+		ChatClient chat = new ChatClient("127.0.0.1", 12);
 		
-		assertEquals(1234, chat.getPortNumber());
+		assertEquals(12, chat.getPortNumber());
 	}
 	
+	@Test
+	public void testError() {
+		Controller c = new Controller("127.0.0.1", 32);
+		View v = new View(c);
+		
+		ChatServer server = new ChatServer(32, v);
+		
+		try {
+			ChatClient chat = new ChatClient("127.0.0.1", 32);
+		} 
+		catch (Exception e) {
+	    String expectedMessage = "Exception caught when trying to listen on port 0 or listening for a connection";
+	    Assert.assertEquals( "Exception message must be correct", expectedMessage, e.getMessage() );
+		}  
+	}
 	
 }
--- S04/src/test/ChatServerTest.java
+++ S04/src/test/ChatServerTest.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import gui.Controller;
 import gui.View;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import connection.ChatServer;
@@ -25,12 +26,19 @@ public class ChatServerTest {
 	
 	@Test
 	public void testError() throws IllegalArgumentException, IOException{
-		Controller c2 = new Controller("localhost", 0);
+		Controller c2 = new Controller("255.255.225.255", 0);
 		View v2 = new View(c2);
 		
 		ChatServer server = new ChatServer(0, v2);
 		
-		assertEquals(0, server.getPortNumber());
+		try {
+			server.run();
+		    Assert.fail( "Should have thrown an exception" );
+		} 
+		catch (Exception e) {
+		    String expectedMessage = "Exception caught when trying to listen on port 0 or listening for a connection";
+		    Assert.assertEquals( "Exception message must be correct", expectedMessage, e.getMessage() );
+		}  
 	}
 
 }
