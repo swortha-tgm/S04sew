bb5b890040b03b8fbcc2c792fadac87fe8f6d34e
sschober-tgm <sschober@student.tgm.ac.at>
2014-11-22 19:36:38 +0100

    Changed IP and Port so that it works

--- S04/src/test/ChatServerTest.java
+++ S04/src/test/ChatServerTest.java
@@ -10,12 +10,12 @@ import connection.ChatServer;
 
 public class ChatServerTest {
 	
-	private final Controller c = new Controller(null, 0);
+	private final Controller c = new Controller("127.0.0.1", 1234);
 	private final View v = new View(c);
 	
 	@Test
 	public void testPortNumber() {
-		ChatServer server = new ChatServer(0, v);
+		ChatServer server = new ChatServer(1234, v);
 		
 		assertEquals(0, server.getPortNumber());
 	}
